70cf39d012cfbc7e4453f1772a64352a1efd72db
Teja Kalapala <teja.1368@gmail.com>
2023-05-27 13:00:56 +0530

    request class

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -9,4 +9,32 @@ import Foundation
 
 final class RMRequest{
     
+    private struct Constants{
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+   private let endpoint:RMEndPoint
+   private let pathComponents:[String]
+   private let queryParameters:[URLQueryItem]
+    private var urlString :String{
+        var string = Constants.baseUrl + "/" + endpoint.rawValue
+        if !pathComponents.isEmpty{
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        if !queryParameters.isEmpty{
+            queryParameters.forEach({
+                string += "/\($0)"
+            })
+        }
+        return string
+    }
+    public var url:URL? {
+        return nil
+    }
+    init(endpoint: RMEndPoint, pathComponents: [String] = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -11,7 +11,7 @@ final class RMService{
     static let shared = RMService()
     private init(){}
     
-    public func execute(_ request:RMRequest,completion:@escaping()->Void){
+    public func execute(_ request:RMRequest,completion:@escaping(Result<Any,Error>)->Void){
         
     }
 }
